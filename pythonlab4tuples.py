ef3134f6b74453aa45bebaa39d9a2cd38fab4619
sufiyatahseen <35676545+sufiyatahseen@users.noreply.github.com>
2018-02-28 08:52:56 +0530

    Add files via upload

---
+++ pythonlab4tuples.py
@@ -0,0 +1,9 @@
+tuple1=('physics','chemistry','biology')
+print(tuple1)
+tuple1=('chemistry')
+tuple2=("1","2","3")
+print(tuple1[0:8])
+print(tuple2[2])
+tuple=('physics','chemistry','biology')
+print(tuple[1])
+
