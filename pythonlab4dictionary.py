ae853aca84af1986151db85d22da5fc5aeb6c0d8
sufiyatahseen <35676545+sufiyatahseen@users.noreply.github.com>
2018-02-28 08:47:45 +0530

    Add files via upload

---
+++ pythonlab4dictionary.py
@@ -0,0 +1,8 @@
+d={1:"hi","py":30}
+print(d[1])
+print(d['py'])
+d={1:"welcome",2:"to",3:"python"}
+print(d[1],d[2],d[3])
+d={1:"my",2:"name",3:"is",4:"sufiya",5:"tahseen"}
+print(d[1],d[2],d[3],d[4],d[5])
+
