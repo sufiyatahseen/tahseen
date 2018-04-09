6d93821d737d343e0bca3e9ae399ea08721874f1
sufiyatahseen <35676545+sufiyatahseen@users.noreply.github.com>
2018-04-09 10:13:49 +0530

    Add files via upload

---
+++ eliff.py
@@ -0,0 +1,11 @@
+marks=int(input("enter the marks"))
+if marks>90:
+    print("Grade A")
+elif marks>80:
+    print("Grade B")
+elif marks>70:
+    print("Grade C")
+elif marks>60:
+    print("Grade D")
+else:
+    print("fail")
